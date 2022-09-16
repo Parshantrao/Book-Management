b47dcf70f27f36eaf682d9b5061b32da74c9f6f8
Parshantrao <parshantrao2017@gmail.com>
2022-09-16 21:18:01 +0530

    Book-Management

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const route = require("./routes/route")
+const app = express()
+const mongoose = require("mongoose")
+
+
+app.use(bodyParser.json())
+
+mongoose.connect(
+    "mongodb+srv://Parshant_rao:C4fIOvHGi74DVINv@newcluster.squkrr6.mongodb.net/InternGroup43",
+    {
+        useNewUrlParser:true
+    }
+).then(()=>console.log("MongoDB is connected"))
+  .catch((err)=>console.log(err))
+
+app.use("/",route)
+
+app.listen(process.env.PORT || 3000, function(){ 
+    console.log("Express is running on port" + (process.env.PORT || 3000))
+}) 
